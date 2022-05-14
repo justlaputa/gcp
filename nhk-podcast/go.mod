3c7db1af4ce20e3ff21716093692529d7215a494
Han <han.xiao@paypay-corp.co.jp>
2022-05-14 20:18:16 +0900

    first push

---
+++ nhk-podcast/go.mod
@@ -0,0 +1,38 @@
+module github.com/justlaputa/gcp/nhk-podcast
+
+go 1.18
+
+require (
+	cloud.google.com/go/storage v1.22.0
+	github.com/mmcdole/gofeed v1.1.3
+	gocloud.dev v0.25.0
+)
+
+require (
+	cloud.google.com/go v0.100.2 // indirect
+	cloud.google.com/go/compute v1.5.0 // indirect
+	cloud.google.com/go/iam v0.3.0 // indirect
+	github.com/PuerkitoBio/goquery v1.5.1 // indirect
+	github.com/andybalholm/cascadia v1.1.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.7 // indirect
+	github.com/google/wire v0.5.0 // indirect
+	github.com/googleapis/gax-go/v2 v2.2.0 // indirect
+	github.com/googleapis/go-type-adapters v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/mmcdole/goxpp v0.0.0-20181012175147-0068e33feabf // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	go.opencensus.io v0.23.0 // indirect
+	golang.org/x/net v0.0.0-20220401154927-543a649e0bdd // indirect
+	golang.org/x/oauth2 v0.0.0-20220309155454-6242fa91716a // indirect
+	golang.org/x/sys v0.0.0-20220330033206-e17cdc41300f // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	google.golang.org/api v0.74.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20220405205423-9d709892a2bf // indirect
+	google.golang.org/grpc v1.45.0 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+)
---
+++ nhk-podcast/handler.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"context"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+	"text/template"
+
+	"github.com/mmcdole/gofeed"
+
+	"cloud.google.com/go/storage"
+	"gocloud.dev/blob"
+
+	//use gcs
+	_ "gocloud.dev/blob/gcsblob"
+)
+
+const (
+	GCP_BUCKET   = "gs://laputa-public"
+	GCP_OBJECT   = "nhk.xml"
+	NHK_FEED_URL = "https://www.nhk.or.jp/s-media/news/podcast/list/v1/all.xml"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	fp := gofeed.NewParser()
+
+	feed, err := fp.ParseURL(NHK_FEED_URL)
+	if err != nil {
+		log.Printf("failed to get and parse nhk rss feed, %w", err)
+		sendError(w, http.StatusInternalServerError)
+		return
+	}
+
+	keepItems := filter(feed.Items)
+	feed.Items = keepItems
+
+	ctx := context.Background()
+
+	b, err := blob.OpenBucket(ctx, GCP_BUCKET)
+	if err != nil {
+		log.Printf("Failed to setup bucket: %s", err)
+		sendError(w, http.StatusInternalServerError)
+	}
+
+	beforeWrite := func(as func(interface{}) bool) error {
+		var sw *storage.Writer
+		if as(&sw) {
+			sw.PredefinedACL = "publicRead"
+		}
+		return nil
+	}
+
+	rssFileWriter, err := b.NewWriter(ctx, GCP_OBJECT, &blob.WriterOptions{
+		ContentType: "application/rss+xml;charset=utf-8",
+		BeforeWrite: beforeWrite,
+	})
+
+	if err != nil {
+		log.Printf("Failed to write rss file to bucket: %s", err)
+		sendError(w, http.StatusInternalServerError)
+	}
+
+	err = writeRss(rssFileWriter, feed)
+
+	if err != nil {
+		log.Printf("failed to generate final rss %w", err)
+		sendError(w, http.StatusInternalServerError)
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func sendError(w http.ResponseWriter, statusCode int) {
+	w.WriteHeader(statusCode)
+}
+
+func filter(items []*gofeed.Item) []*gofeed.Item {
+	containsAny := func(s string, keys []string) bool {
+		for _, k := range keys {
+			if strings.Contains(s, k) {
+				return true
+			}
+		}
+
+		return false
+	}
+
+	titleFilterKeys := []string{"午前７時", "夜７時"}
+
+	keepItems := []*gofeed.Item{}
+
+	for _, item := range items {
+		if containsAny(item.Title, titleFilterKeys) {
+			log.Printf("found item to keep: %#v", item)
+			keepItems = append(keepItems, item)
+		}
+	}
+
+	return keepItems
+}
+
+func writeRss(wr io.Writer, feed *gofeed.Feed) error {
+	t, err := template.ParseFiles("./nhk-feed.template")
+	if err != nil {
+		return err
+	}
+
+	t.Execute(wr, feed)
+
+	return nil
+}
